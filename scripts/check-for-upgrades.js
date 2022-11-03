3bd6b979f5352867249a0fb7bdb49cfca6e15d45
team-tf-cdk <github-team-tf-cdk@hashicorp.com>
2022-11-03 00:18:36 +0000

    chore(deps): upgrade dependencies
    Upgrades project dependencies. See details in [workflow run].
    
    [Workflow Run]: https://github.com/cdktf/cdktf-provider-salesforce/actions/runs/3382080891
    
    ------
    
    *Automatically created by projen via the "upgrade-main" workflow*
    
    Signed-off-by: team-tf-cdk <github-team-tf-cdk@hashicorp.com>

--- scripts/check-for-upgrades.js
+++ scripts/check-for-upgrades.js
@@ -11,7 +11,7 @@ const actions = require("@actions/core");
 
 // set by the projen file that generates this script
 const CONSTRAINT = "~> 0.1";
-const FQ_PROVIDER_NAME = "salesforce";
+const FQ_PROVIDER_NAME = "hashicorp/salesforce";
 
 (async function main() {
   if (await newerMatchingProviderVersionExists()) {
